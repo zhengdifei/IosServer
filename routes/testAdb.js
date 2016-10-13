59600217e0c3c1e4e589edcf2bbf11f2f86d5eb8
zhengdifei <zhengdifeixp@126.com>
2016-10-13 14:38:02 +0800

    接口测试类

--- routes/testAdb.js
+++ routes/testAdb.js
@@ -4,15 +4,21 @@ const adb = require('../client/adb').createClient({host:'localhost',port:5078})
     console.log(data)
 })*/
 
-adb.trackDevices(function(){
-    console.log('find device end')
+adb.trackDevices(function(err,tracker){
+    console.log('find device')
 }).then(function(tracker){
     tracker.on("add",function(){
         console.log("add device")
-        adb.listDevices(function(){
-            console.log('list device end')
+        adb.listDevices(function(err,serial){
+            console.log("show device serial")
         }).then(function(data){
             console.log("serialId:",data)
         })
     })
 })
+
+//adb.getProperties('zdf',function(properties){
+//    console.log('1',properties)
+//}).then(function(properties){
+//    console.log('2',properties)
+//})
